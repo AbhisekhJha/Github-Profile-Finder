16a3ed4905bc673af4dbb8a85c7b81e476621a39
MeAbhiJha <iamabhisekh11@gmail.com>
2026-01-08 17:18:29 +0545

    feat:implement github profile and display content

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import { useGithub } from './composables/useGithub'
+import SearchGithubUser from './components/SearchGithubUser.vue'
+import ProfileCard from './components/ProfileCard.vue'
+
+const { profile, loading, fetchUser } = useGithub()
+
+function handleSearch(username: string): void {
+  fetchUser(username)
+}
 </script>
 
 <template>
-  <div class="min-h-screen flex items-center justify-center bg-slate-900 text-white">
-    <h1 class="text-4xl font-bold">Hello Vue 3 + TS + Tailwind</h1>
+  <div class="max-w-2xl mx-auto p-4">
+     <h1 class="text-blue-500 text-3xl font-bold text-center mb-6">
+      GitHub Profile Finder
+    </h1>
+    <SearchGithubUser :onSearch="handleSearch" />
+
+    <p v-if="loading" class="text-gray-500 mt-2">Loading...</p>
+   
+
+    <ProfileCard :profile="profile" />
   </div>
 </template>
 
