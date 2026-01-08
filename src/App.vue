be6ce930e4f5234bf07d132a9b297b4333499824
MeAbhiJha <iamabhisekh11@gmail.com>
2026-01-08 12:29:54 +0545

    Initial clean Vue 3 + TS + Tailwind project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+    <h1 class="text-4xl font-bold">Hello Vue 3 + TS + Tailwind</h1>
+  </div>
+</template>
+
